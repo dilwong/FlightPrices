8988fae9487aba0e88f106d869bf94e4a8554037
Matheus Borges <matheusbfaria@gmail.com>
2023-04-08 16:41:09 -0300

    Cron and crontab setup

--- setup/setup_linux_environment.sh
+++ setup/setup_linux_environment.sh
@@ -1,7 +1,14 @@
 # Setup built on a Fedora Linux distribution
 
-sudo dnf install tmux python
+# Install useful packages
+sudo dnf install tmux python cronie
 pip install jupyterlab
+
+# Start cron service
+sudo systemctl start crond.service
+
+# Enable the cron service so that it starts automatically during system startup
+sudo systemctl enable crond.service
 
 # Set new tmux config
 chmod +x change_tmux_config.sh
