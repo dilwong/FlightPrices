0d3df9b6f9e07f8537bd64c24e60351ac09af4c3
Matheus Borges <matheusbfaria@gmail.com>
2023-04-23 23:48:23 -0300

    Create a method that checks if the machine should run at a certain time

--- scrape/coordinate_scraper.py
+++ scrape/coordinate_scraper.py
@@ -25,32 +25,34 @@ class CoordinateScraper():
         self.machines_number = machines_number
         self.machines_per_date = machines_per_date
         self.complete_relay_cycle = None
-
+        self.timesheet = None
         
-#     def check_should_run_hour(self, date, machine_id):
-#         """Checks if the machine should run in a hour.
+    def check_should_run_hour(self, date, machine_id):
+        """Checks if the machine should run in a hour.
         
-#         Parameters
-#         ----------
-#         date: datetime.datetime
-#             Date to check.
-#         machine_id: int
-#             The number that identifies the machine.
+        Parameters
+        ----------
+        date: datetime.datetime
+            Date to check.
+        machine_id: int
+            The number that identifies the machine.
         
-#         Return
-#         ------
-#         should_run: bool
-#             True if the machine should run, False otherwise.
-#         """
-#         self.__assert_machine_values(machine_id)
-#         self.__assert_date(date)
-#         should_run = self.check_should_run_date(date, machine_id)
-#         if should_run:
-#             cycle_date_index = self.get_cycle_date_index(date)
-#             delta_time = date - self.base_date
-#             # delta_time.
-#             pass
-#         return should_run
+        Return
+        ------
+        should_run: bool
+            True if the machine should run, False otherwise.
+        """
+        self.__assert_machine_values(machine_id)
+        self.__assert_date(date)
+        should_run = self.check_should_run_date(date, machine_id)
+        if should_run:
+            cycle_date_index = self.get_cycle_date_index(date)
+            day_hours = 24
+            max_index = cycle_date_index * day_hours
+            min_index = max_index - day_hours
+            day_timesheet = self.get_timesheet()
+            should_run = day_timesheet[date.hour] == machine_id
+        return should_run
         
     
     def check_should_run_date(self, date, machine_id):
@@ -71,10 +73,68 @@ class CoordinateScraper():
         self.__assert_machine_values(machine_id)
         self.__assert_date(date)
         
-        
         machines_run_date = self.get_machines_run_date(date)
         should_run = machine_id in machines_run_date
         return should_run
+    
+    def get_cycle_date_index(self, date):
+        """Get the index of the cycle that the date belongs to.
+        
+        Parameters
+        ----------
+        date: datetime.datetime
+            Date to check.
+        
+        Return
+        ------
+        cycle_date_index: int
+            Day of the cycle that the date belongs to.
+            0 <= cycle_date_index < self.machines_number
+        """
+        complete_relay_cycle = self.get_complete_relay_cycle()
+        delta_time = date - self.base_date
+        cycle_date_index = delta_time.days % len(complete_relay_cycle) - 1
+        return cycle_date_index
+    
+    def get_timesheet(self):
+        """Get the machines timesheet.
+        
+        Return
+        ------
+        timesheet: list[int]
+            It is the list that keeps which machine_id must be run in each hour of a cycle.
+            Example: index 0 of the list stores the machine_id that must run at hour 0 of
+            the first day of the cycle; index 24 stores the machine_id that must run at
+            hour 0 of the second day of the cycle.
+        """
+        if self.timesheet is None:
+            day_hours = 24
+            self.timesheet = list()
+            for machines_id in self.get_complete_relay_cycle():
+                repetitions = day_hours//len(machines_id) + 1
+                day_timesheet = machines_id * repetitions
+                if len(self.timesheet) > 0:
+                    if day_timesheet[0] == self.timesheet[-1]:
+                        day_timesheet.pop(0)
+                self.timesheet +=  day_timesheet[0:day_hours]
+        return self.timesheet
+    
+    def get_machines_run_date(self, date):
+        """Get the machines that must run on a date.
+        
+        Parameters
+        ----------
+        date: datetime.datetime
+            Date to check.
+        
+        Return
+        ------
+        machines_run_date: list
+            Machine that should run.
+        """
+        cycle_date_index = self.get_cycle_date_index(date)
+        machines_run_date = self.get_complete_relay_cycle()[cycle_date_index]
+        return machines_run_date
         
     def get_complete_relay_cycle(self):
         """Get the complete cycle of the relay machine.
@@ -97,49 +157,13 @@ class CoordinateScraper():
                     machines_list = list(range(first_machine, self.machines_number+1)) + list(range(1, last_machine+1))
                 complete_relay_cycle.append(machines_list)
                 self.complete_relay_cycle = complete_relay_cycle
-        return self.complete_relay_cycle
-
-    def get_machines_run_date(self, date):
-        """Get the machines that must run on a date.
-        
-        Parameters
-        ----------
-        date: datetime.datetime
-            Date to check.
-        
-        Return
-        ------
-        machines_run_date: list
-            Machine that should run.
-        """
-        cycle_date_index = self.get_cycle_date_index(date)
-        machines_run_date = self.get_complete_relay_cycle()[cycle_date_index]
-        return machines_run_date
-
-    def get_cycle_date_index(self, date):
-        """Get the index of the cycle that the date belongs to.
-        
-        Parameters
-        ----------
-        date: datetime.datetime
-            Date to check.
-        
-        Return
-        ------
-        cycle_date_index: int
-            Day of the cycle that the date belongs to.
-            0 <= cycle_date_index < self.machines_number
-        """
-        complete_relay_cycle = self.get_complete_relay_cycle()
-        delta_time = date - self.base_date
-        cycle_date_index = delta_time.days % len(complete_relay_cycle) - 1
-        return cycle_date_index
+        return self.complete_relay_cycle    
 
     def __assert_machine_values(self, machine_id):
         machine_values = list(range(1, self.machines_number+1))
         assert machine_id in machine_values, (
             f"machine_id should be one of the values {machine_values},"
-            f"but machine_id={machine_id}")
+            f" but machine_id={machine_id}")
 
     def __assert_date(self, date):
         assert date > self.base_date, (f"date shold be bigger "
